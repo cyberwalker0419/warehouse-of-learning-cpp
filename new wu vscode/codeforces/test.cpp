e51a82e536661928a9cab57b3bb053750c94cd45
cyberwalker0419 <121590762+cyberwalker0419@users.noreply.github.com>
2023-09-24 16:23:24 +0800

    Create test.cpp

---
+++ new wu vscode/codeforces/test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define N 100005
+#define INF 0x7fffffff
+#define INFLL 0x7fffffffffffffff
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cout<<"hello world!";
+    return 0;
+}
